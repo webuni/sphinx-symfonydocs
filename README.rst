5985de7d2fee3c63c31be5ec96a64c6a478c6705
Martin Hasoň <martin.hason@gmail.com>
2013-07-26 14:24:58 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,22 @@
+Sphinx extension for Symfony documentation
+==========================================
+
+Installation
+------------
+
+Install via pip:
+
+    $ pip install https://github.com/webuni/sphinx-symfonydocs
+
+or if you have the code checked out locally:
+
+    $ python setup.py install
+
+Configuration
+-------------
+
+Add the following to your conf.py:
+
+.. code-blocK:: python
+
+    extension = ['webuni.sphinx.symfonydocs']
---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'webuni.sphinx.symfonydocs',
+    version = '0.8',
+    author = u'Martin Hasoň',
+    author_email = 'martin.hason@gmail.com',
+    description = 'Webuni Sphinx extension for Symfony documentation',
+    license = 'MIT',
+    platforms = 'any',
+    packages = find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+    install_requires = [
+        'Sphinx>=1.1',
+        'Jinja2>=2.7',
+        'requests>=1.0',
+        'sphinx-php>=1.0'
+    ],
+    dependency_links = [
+        'https://github.com/fabpot/sphinx-php/archive/master.zip#egg=sphinx-php-1.0'
+    ]
+)
